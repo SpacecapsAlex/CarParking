cbe5fb79bbeb1329b0f4e679df20b4bc8fa34c5f
SpacecapsAlex <spacecapsalex@hotmail.com>
2024-02-15 21:38:49 +0300

    register models on admin

--- autopark/admin.py
+++ autopark/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Car, CarType, CarBrand
 
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(CarType)
+admin.site.register(CarBrand)
