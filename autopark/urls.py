40f2e5b731f1c81c14f5c6b00e1efd9dcc91ba5c
SpacecapsAlex <spacecapsalex@hotmail.com>
2024-02-26 19:29:25 +0300

    Add redirect views to URL configurations
    The URL configurations in 'CarParking' and 'autopark' apps have been updated with additional redirect views. These redirect views will essentially redirect the blank path ('') to the 'auto-park/' and 'get-cars/' paths in their respective apps, generally streamlining access to primary features.

--- autopark/urls.py
+++ autopark/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='get-cars/', permanent=True)),
     path('inititialize/', views.initialize, name='initialize'),
     path('get-cars/', views.get_cars, name='get-cars'),
     path('get-car-by-id/<int:car_id>/', views.get_car_by_id, name='get-car-by-id'),
