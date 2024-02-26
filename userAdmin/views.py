ea9481b9593abb42a9071eaeb29fac326f449c1f
SpacecapsAlex <spacecapsalex@hotmail.com>
2024-02-26 21:23:32 +0300

    Implement user signup feature
    The code has been updated to include functionality for a user signup feature. A new user can now input their username, email, and password, which will be stored in the User table. The user is then assigned to the 'user' group and the details are saved to the database.

--- userAdmin/views.py
+++ userAdmin/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import User, Group
 
 
 def signup(request: HttpRequest):
     if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        group_user = Group.objects.get(name='user')
+
+        user = User.objects.create_user(username, email, password)
+        user.groups.add(group_user)
+        user.save()
+
         return HttpResponse('Signup')
     else:
         return render(request, 'userAdmin/signup.html')
